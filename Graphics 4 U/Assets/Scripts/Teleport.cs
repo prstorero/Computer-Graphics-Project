2020fcd8d0741f8e73d90e7f9d4f999d9bc123f6
scott johnson <prssecustom94@gmail.com>
2017-11-30 20:38:03 -0600

    Roller ball integration

--- Graphics 4 U/Assets/Scripts/Teleport.cs
+++ Graphics 4 U/Assets/Scripts/Teleport.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Teleport : MonoBehaviour {
 
 	private GameObject player;
+
+	// Portal variables
 	private GameObject homePortalTopic1;
+	private GameObject homePortalTopic2;
 	private GameObject topic1Portal;
+	private GameObject topic2Portal;
+
+	// Topic 2 variables
+	private GameObject topic2Canvas;
+	private GameObject pickups;
+	private bool isTopic2 = false;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindWithTag("Player");
 		homePortalTopic1 = GameObject.FindWithTag("homePortalTopic1");
+		homePortalTopic2 = GameObject.FindWithTag("homePortalTopic2");
 		topic1Portal = GameObject.FindWithTag("topic1Portal");
-		
+		topic2Portal = GameObject.FindWithTag("topic2Portal");
+		topic2Canvas = GameObject.FindWithTag("topic2Canvas");
+		pickups = GameObject.FindWithTag("pickups");
+
+		topic2Canvas.SetActive(false);
+		pickups.SetActive(false);
+
 	}
 
 	private void OnCollisionEnter(Collision collision)
@@ -28,14 +44,31 @@ public class Teleport : MonoBehaviour {
 			case "homePortalTopic1":
 				player.transform.position = topic1Portal.transform.position + Vector3.back;
 				break;
+			case "homePortalTopic2":
+				player.transform.position = topic2Portal.transform.position + Vector3.back;
+				topic2Canvas.SetActive(true);
+				pickups.SetActive(true);
+				isTopic2 = true;
+				player.SetActive(false);
+				break;
 			case "topic1Portal":
 				player.transform.position = homePortalTopic1.transform.position + Vector3.forward;
+				break;
+			case "topic2Portal":
+				player.transform.position = homePortalTopic2.transform.position + Vector3.forward;
+				topic2Canvas.SetActive(false);
+				pickups.SetActive(false);
 				break;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (isTopic2 && Input.GetKey(KeyCode.Escape))
+		{
+			isTopic2 = false;
+			player.SetActive(true);
+		}
 	}
 }
